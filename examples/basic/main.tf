bb45314d46adf5d96e996b4ba3f1100e08ae8b34
vikram <francisvikramsagar@gmail.com>
2021-08-12 20:32:48 +0530

    m

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-2"
 }
 
-
-
 ##################################################################
 # Data sources to get VPC, subnet, security group and AMI details
 ##################################################################
@@ -15,7 +13,7 @@ data "aws_subnet_ids" "all" {
   vpc_id = data.aws_vpc.default.id
 }
 
-data "aws_ami" "amazon_linux" {
+data "aws_ami" "ubuntu" {
   most_recent = true
 
   owners = ["amazon"]
@@ -50,141 +48,30 @@ module "security_group" {
   egress_rules        = ["all-all"]
 }
 
-
-
-resource "aws_placement_group" "web" {
-  name     = "hunky-dory-pg"
-  strategy = "cluster"
-}
-
-#resource "aws_kms_key" "this" {
-#}
-
-#resource "aws_network_interface" "this" {
-  count = 1
-
-  subnet_id = tolist(data.aws_subnet_ids.all.ids)[count.index]
-#}
-
-#module "ec2" {
+module "ec2" {
   source = "../../"
 
-  instance_count = 1
+  instance_count = var.instances_number
 
-  name          = "example-normal"
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "c5.large"
-  subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
-  #  private_ips                 = ["172.31.32.5", "172.31.46.20"]
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-  placement_group             = aws_placement_group.web.id
-
-  user_data_base64 = base64encode(local.user_data)
-
-  enable_volume_tags = false
-  root_block_device = [
-    {
-      volume_type = "gp2"
-      volume_size = 10
-      tags = {
-        Name = "my-root-block"
-      }
-    },
-  ]
-
-  ebs_block_device = [
-    {
-      device_name = "/dev/sdf"
-      volume_type = "gp2"
-      volume_size = 5
-      encrypted   = true
-      kms_key_id  = aws_kms_key.this.arn
-    }
-  ]
-
-  tags = {
-    "Env"      = "Private"
-    "Location" = "Secret"
-  }
-#}
-
-module "ec2_with_t2_unlimited" {
-  source = "../../"
-
-  instance_count = 1
-
-  name          = "example-t2-unlimited"
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
-  cpu_credits   = "unlimited"
-  subnet_id     = tolist(data.aws_subnet_ids.all.ids)[0]
-  #  private_ip = "172.31.32.10"
+  name                        = "example-with-ebs"
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t2.micro"
+  subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
   vpc_security_group_ids      = [module.security_group.security_group_id]
   associate_public_ip_address = true
 }
 
-#module "ec2_with_t3_unlimited" {
-  source = "../../"
+resource "aws_volume_attachment" "this" {
+  count = var.instances_number
 
-  instance_count = 1
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.this[count.index].id
+  instance_id = module.ec2.id[count.index]
+}
 
-  name                        = "example-t3-unlimited"
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t3.large"
-  cpu_credits                 = "unlimited"
-  subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-#}
+resource "aws_ebs_volume" "this" {
+  count = var.instances_number
 
-#module "ec2_with_metadata_options" {
-  source = "../../"
-
-  instance_count = 1
-
-  name                        = "example-metadata_options"
-  ami                         = data.aws_ami.amazon_linux.id
-  instance_type               = "t2.small"
-  subnet_id                   = tolist(data.aws_subnet_ids.all.ids)[0]
-  vpc_security_group_ids      = [module.security_group.security_group_id]
-  associate_public_ip_address = true
-
-  metadata_options = {
-    http_endpoint               = "enabled"
-    http_tokens                 = "required"
-    http_put_response_hop_limit = 8
-  }
-#}
-
-#module "ec2_with_network_interface" {
-  source = "../../"
-
-  instance_count = 1
-
-  name            = "example-network"
-  ami             = data.aws_ami.amazon_linux.id
-  instance_type   = "c5.large"
-  placement_group = aws_placement_group.web.id
-
-  network_interface = [
-    {
-      device_index          = 0
-      network_interface_id  = aws_network_interface.this[0].id
-      delete_on_termination = false
-    }
-  ]
-#}
-
-# This instance won't be created
-#module "ec2_zero" {
-  source = "../../"
-
-  instance_count = 0
-
-  name                   = "example-zero"
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "c5.large"
-  subnet_id              = tolist(data.aws_subnet_ids.all.ids)[0]
-  vpc_security_group_ids = [module.security_group.security_group_id]
-#}
+  availability_zone = module.ec2.availability_zone[count.index]
+  size              = 1
+}
