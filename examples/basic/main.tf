f4e2f6b2de4100f74ceda756b5b8f3fecb3638d7
vikram <francisvikramsagar@gmail.com>
2021-08-12 20:45:10 +0530

    m

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -16,21 +16,13 @@ data "aws_subnet_ids" "all" {
 data "aws_ami" "amazon_linux" {
   most_recent = true
 
-  owners = ["amazon"]
+  owners = ["361260017265"]
 
   filter {
     name = "name"
 
     values = [
       "amzn-ami-hvm-*-x86_64-gp2",
-    ]
-  }
-
-  filter {
-    name = "owner-alias"
-
-    values = [
-      "amazon",
     ]
   }
 }
